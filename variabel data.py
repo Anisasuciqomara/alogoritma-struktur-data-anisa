5cba3d053c60a16862ffd8328dfdb32804b58dc2
Anisasuciqomara <anisasuciq@gmail.com>
2024-09-27 18:07:51 +0700

    Add files via upload

---
+++ variabel data.py
@@ -0,0 +1,9 @@
+# variable a merepresentasikan panjang
+a = 5
+b = 10 # variabel b mempresentasikan tinggi
+
+"""
+     dan variabel c mempresentasikan luas  persegi dari hasil  perkalian variabel a dan variabel c
+     
+"""
+c = a * b
